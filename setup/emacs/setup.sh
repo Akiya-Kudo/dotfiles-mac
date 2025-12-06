51f92af7d23773e80bf5c10a64d2a6ba83ef3f1e
Akiya-Kudo <k.akiya07@gmail.com>
2025-12-07 03:50:48 +0900

    emacs.d

--- setup/emacs/setup.sh
+++ setup/emacs/setup.sh
@@ -6,6 +6,13 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 DOTFILES_DIR="$(cd "$SCRIPT_DIR/../.." && pwd)"
 
+# set ~/.emacs.d/init.el
+mkdir -p $HOME/.emacs.d
+if [ -e "$HOME/.emacs.d/init.el" ]; then
+    ln -s "$SCRIPT_DIR/init.el" "$HOME/.emacs.d/init.el"
+fi
+
+# set emacs configuration (init.el)
 mkdir -p $HOME/.config
 if [ ! -e "$HOME/.config/emacs" ]; then
     ln -s "$DOTFILES_DIR/.config/emacs" "$HOME/.config"
