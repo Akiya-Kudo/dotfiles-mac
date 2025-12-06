b10dc786da45f3430e5716cae52da315ddde82c1
Akiya-Kudo <k.akiya07@gmail.com>
2025-12-06 15:43:49 +0900

    fix lua

--- .config/wezterm/lua/color.lua
+++ .config/wezterm/lua/color.lua
@@ -4,8 +4,8 @@ function module.apply_to_config(config)
     config.background = '#000000'
     config.foreground = '#ffffff'
     config.color_scheme = 'iceberg-dark'
-    config.window_background_opacity = 0.3,
-    config.macos_window_background_blur = 15,
+    config.window_background_opacity = 0.3
+    config.macos_window_background_blur = 15
 end
 
 -- return our module table
